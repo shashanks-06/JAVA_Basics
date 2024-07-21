2c10ca2a364239e784743e0084e929f1371b6ade
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-21 21:11:10 +0530

    7th July 2024

---
+++ src/ex_07072024/Lab005.java
@@ -0,0 +1,13 @@
+package ex_07072024;
+
+public class Lab005 {
+    // Can I run class without a main method?
+    // run option?
+    public static void main(String[] args) {
+
+    }
+
+    public static void main() {
+
+    }
+}
