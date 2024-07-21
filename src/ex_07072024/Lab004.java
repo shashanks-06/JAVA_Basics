411e5c0fd5bd16690b35b0c5e302a6fa360954c3
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-21 21:09:21 +0530

    7th July 2024

---
+++ src/ex_07072024/Lab004.java
@@ -0,0 +1,12 @@
+package ex_07072024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        System.out.print("Hello World!");
+        System.out.print("Hello World!");
+
+    }
+}
