6a3f729606c86266249976968ac2d46e3b5c2b44
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-22 11:43:32 +0530

    13 July 2024

---
+++ src/ex_13072024/Lab017.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+
+public class Lab017 {
+    public static void main(String[] args) {
+        final int A =99;
+//        A=100; // java: cannot assign a value to final variable A
+        System.out.println(A);
+
+        final String str = "yuastd";
+        final double d = 378.155;
+    }
+}
