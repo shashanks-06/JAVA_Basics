e49cf42cbdf0577abac9ac3bc300dab32adc0ad7
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-22 11:30:31 +0530

    13 July 2024

---
+++ src/ex_13072024/Lab013.java
@@ -0,0 +1,23 @@
+package ex_13072024;
+
+public class Lab013 {
+    public static void main(String[] args) {
+//        int a =35;
+//        int b=45;
+//        System.out.println(a+b);
+//
+//        String s1 = "Shashank";
+//        String s2 = "Surjekar";
+//        System.out.println(s1+s2);
+
+        String s3 = "John";
+        int x = 10;
+        int y = 30;
+
+//        System.out.println(x+y+s3);
+//        System.out.println(x+s3+y);
+//        System.out.println(s3+x+y);
+
+        System.out.println(x+y+s3+y+s3+x+y);
+    }
+}
