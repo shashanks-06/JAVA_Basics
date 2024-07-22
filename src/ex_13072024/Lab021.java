cec19be897449529cb5546d6df63bade64b9c302
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-22 12:07:05 +0530

    13 July 2024

---
+++ src/ex_13072024/Lab021.java
@@ -0,0 +1,15 @@
+package ex_13072024;
+
+public class Lab021 {
+    public static void main(String[] args) {
+
+        int age_s = 23;
+        int age_m = 23;
+
+//        boolean result = age_s > age_m;
+//        boolean result = age_s < age_m;
+        boolean result = age_s >= age_m; // OR Gate
+
+        System.out.println(result);
+    }
+}
