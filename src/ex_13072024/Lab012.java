636e8b865fc20afc487b153b861d78c359469d2d
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-22 11:24:06 +0530

    13 July 2024

---
+++ src/ex_13072024/Lab012.java
@@ -0,0 +1,16 @@
+package ex_13072024;
+
+public class Lab012 {
+    public static void main(String[] args) {
+        // Formatting
+        int age = 99;
+        System.out.println("Your Age is -> " +age);
+
+        // %d ->  any integer - byte, short, int or long
+        // %s -> String
+        // %c -> char
+        // %f -> float
+
+        System.out.printf("Your age is -> %d",age);
+    }
+}
