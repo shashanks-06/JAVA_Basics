f6af8f68522ee75bcc0f5d31ef9cab0398978a2c
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-22 18:09:44 +0530

    14 July 2024

---
+++ src/ex_14072024/Lab030.java
@@ -0,0 +1,16 @@
+package ex_14072024;
+
+public class Lab030 {
+    public static void main(String[] args) {
+        int course = 300;
+        float gst = 18.76f;
+
+//        int totalPrice = course+ gst;  // Implicit Narrowing - JVM
+
+        int totalPrice = course + (int)gst; // Explit narrowing - REAL time - money loss
+        System.out.println(totalPrice); // 318
+
+        float floatingTotalPrice = course+ gst;
+        System.out.println(floatingTotalPrice); // 318.76
+    }
+}
