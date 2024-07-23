8cafe0dc22fb04c6b69dedc417ef5a08caac06e3
shashanks-06 <surjekarshashank@gmail.com>
2024-07-23 17:57:10 +0530

    20 July 2024

---
+++ src/ex_20072024/Lab043.java
@@ -0,0 +1,21 @@
+package ex_20072024;
+
+import java.util.Scanner;
+
+public class Lab043 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Enter Number 1");
+        int num1 = cin.nextInt();
+        System.out.println("Enter Number 2");
+        int num2 = cin.nextInt();
+
+        if(num1 > num2){
+            System.out.println(num1);
+        } else if (num2 > num1) {
+            System.out.println(num2);
+        }else {
+            System.out.println("Equal");
+        }
+    }
+}
