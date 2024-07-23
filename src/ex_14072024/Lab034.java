3df80caa627c86e233073c869a377cac2792e710
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-23 10:43:22 +0530

    14 July 2024

---
+++ src/ex_14072024/Lab034.java
@@ -0,0 +1,22 @@
+package ex_14072024;
+
+public class Lab034 {
+    public static void main(String[] args) {
+        String name = "Shashank";
+        // String? - // Bunch of Chars - Collection of Chars
+        // class - ?
+        String name2 = new String("Shashank");
+        // How many ways we can form a String - 2
+        // = , new operator
+        // = "SCP" // String constant Pool
+        // new - Objects(heap)
+
+        System.out.println(name.toLowerCase());
+        System.out.println(name.length());  // 8
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(0));     //S
+        System.out.println(name.charAt(2)); // a
+        System.out.println(name.charAt(10)); // Index 10 out of bounds for length 8
+    }
+
+}
