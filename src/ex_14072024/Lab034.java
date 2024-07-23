0b399393e097fa66c130133df0d8b09743b03d15
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-23 12:11:05 +0530

    14 July 2024

--- src/ex_14072024/Lab034.java
+++ src/ex_14072024/Lab034.java
@@ -2,14 +2,18 @@ package ex_14072024;
 
 public class Lab034 {
     public static void main(String[] args) {
-        String name = "Shashank";
+        String name = "Shashank";                               // Assignment Operators
         // String? - // Bunch of Chars - Collection of Chars
         // class - ?
-        String name2 = new String("Shashank");
+        String name2 = new String("Shashank");          // New Operator
         // How many ways we can form a String - 2
         // = , new operator
         // = "SCP" // String constant Pool
         // new - Objects(heap)
+
+        // Both of them, actually save the name, name2 in different ways JVM
+        // How they store the values in the JVM?
+        // SCP and Heap - JVM
 
         System.out.println(name.toLowerCase());
         System.out.println(name.length());  // 8
