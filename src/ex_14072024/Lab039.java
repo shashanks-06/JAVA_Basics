f3e3d15f634337f1863daf313b54c250b41d136c
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-23 12:35:03 +0530

    14 July 2024

---
+++ src/ex_14072024/Lab039.java
@@ -0,0 +1,14 @@
+package ex_14072024;
+
+public class Lab039 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 45;
+
+        int max = a > b ? a : b;
+        int min = a < b ? a : b;
+
+        System.out.println("Max is " + max);
+        System.out.println("Min is " + min);
+    }
+}
