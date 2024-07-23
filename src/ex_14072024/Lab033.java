bc9e5b0de97688a03565ddbe4968db4ebfb5a4ad
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-07-23 10:36:55 +0530

    14 July 2024

---
+++ src/ex_14072024/Lab033.java
@@ -0,0 +1,14 @@
+package ex_14072024;
+
+public class Lab033 {
+    public static void main(String[] args) {
+        int a = 86;
+        int b = 92;
+        String s1 = "KK";
+        String s2 = "VV";
+
+        System.out.println(s1+s2+a+b);      //KKVV8692
+        System.out.println(s1+s2+(a+b));    //KKVV178
+
+    }
+}
