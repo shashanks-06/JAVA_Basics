56dcb32c3076917874c06f2ad179f81a0283ef7a
shashanks-06 <surjekarshashank@gmail.com>
2024-07-24 10:55:09 +0530

    20 July 2024

---
+++ src/HomeWork/TriangleClassifier.java
@@ -0,0 +1,21 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class TriangleClassifier {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Enter 3 sides of Triangle : ");
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+        int c = cin.nextInt();
+
+        if(a == b && b == c && a == c){
+            System.out.println("This is a Equilateral Traingle");
+        } else if (a == c || b == c || a == b) {
+            System.out.println("This is a Isosceles Triangle");
+        }else{
+            System.out.println("This is a Scalene Triangle");
+        }
+    }
+}
