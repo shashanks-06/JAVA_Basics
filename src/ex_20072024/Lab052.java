652ca2e034bc2c61bccb17fe7925effe9f523447
shashanks-06 <surjekarshashank@gmail.com>
2024-07-24 13:16:46 +0530

    20 July 2024

--- src/ex_20072024/Lab052.java
+++ src/ex_20072024/Lab052.java
@@ -8,7 +8,7 @@ public class Lab052 {
 
         int val = switch (code){
             case 'A':
-                yield  65;
+                yield  65;  // return
             case 'B':
                 yield  66;
             default:
