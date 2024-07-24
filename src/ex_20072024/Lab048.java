d887a8316e13b987c39631b43645b88424a3044f
shashanks-06 <surjekarshashank@gmail.com>
2024-07-24 11:33:33 +0530

    20 July 2024

--- src/ex_20072024/Lab048.java
+++ src/ex_20072024/Lab048.java
@@ -1,4 +1,32 @@
 package ex_20072024;
 
+import java.util.Scanner;
+
 public class Lab048 {
+    public static void main(String[] args) {
+        Scanner cin =  new Scanner(System.in);
+        System.out.println("Enter a character to know it is  VOWEL or not");
+        char userInp = cin.next().charAt(0);
+
+
+        switch (userInp){
+            case 'a':
+                System.out.println("VOWEL");
+                break;
+            case 'e':
+                System.out.println("VOWEL");
+                break;
+            case 'i':
+                System.out.println("VOWEL");
+                break;
+            case 'o':
+                System.out.println("VOWEL");
+                break;
+            case 'u':
+                System.out.println("VOWEL");
+                break;
+            default:
+                System.out.println("CONSONANT");
+        }
+    }
 }
