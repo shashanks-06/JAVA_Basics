b3d73411ca54e61cf94771e0597732af8a7d7d16
shashanks-06 <surjekarshashank@gmail.com>
2024-07-24 12:59:38 +0530

    20 July 2024

---
+++ src/ex_20072024/Lab051.java
@@ -0,0 +1,14 @@
+package ex_20072024;
+
+public class Lab051 {
+    public static void main(String[] args) {
+        int itemCode = 006;
+
+        switch (itemCode){
+            case 001 -> System.out.println("It is a laptop");
+            case 002 -> System.out.println("It is a desktop");
+            case 003,004 -> System.out.println("It is a mobile");
+            default -> System.out.println("Not a Device");
+        }
+    }
+}
