e04425319f205444a3cc7656be802a68cef7d91f
shashanks-06 <surjekarshashank@gmail.com>
2024-07-25 13:14:24 +0530

    20 July 2024

---
+++ src/ex_21072024/Lab058.java
@@ -0,0 +1,14 @@
+package ex_21072024;
+
+public class Lab058 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(i);
+            if ( i == 5){
+                continue;
+            }
+            System.out.println("After!!");
+
+        }
+    }
+}
