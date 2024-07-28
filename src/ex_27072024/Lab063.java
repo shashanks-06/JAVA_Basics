4e673f07b1e41d73eb4fbe0836af1c7ccefb1c51
shashanks-06 <surjekarshashank@gmail.com>
2024-07-28 16:28:29 +0530

    21 July 2024

---
+++ src/ex_27072024/Lab063.java
@@ -0,0 +1,34 @@
+package ex_27072024;
+
+public class Lab063 {
+    public static void main(String[] args) {
+        // dataType[] arrayName;
+        // Data Types
+        // Primitive
+        //  byte, short, int, long, double, float, char, boolean
+        // Non Primitive
+        // String, Class, enum
+
+        int[] marks = {99,97,95,92,96,81};
+        // elements are fixed.
+        // len -> 6
+        System.out.println(marks.length);
+        System.out.println(marks[5]);
+        System.out.println(marks[0]);
+//        System.out.println(marks[-1]);  //ArrayIndexOutOfBoundsException: Index -1 out of bounds for length 6
+//        System.out.println(marks[10]);  //ArrayIndexOutOfBoundsException: Index -1 out of bounds for length 6
+
+
+
+
+//        int[] marks = {99,97,95,92,"Ninety-Six",81};  // Not Possible
+        float[] gstPaid = {18.65f, 56.23f, 23.6f, 89.12f};
+        String[] shoppingList = {"Bread", "Butter","Milk", "Biscuit"};
+
+        char [] grades = {'A','B','C','D','E','F'};
+        char [] grades_small = {'a','c','C','D','e','F', '#','@','%'};
+        // camelCase = shashanksurjekar -> shashankSurjekar
+        // greetme -> greetMe
+
+    }
+}
---
+++ src/ex_27072024/Lab064.java
@@ -0,0 +1,34 @@
+package ex_27072024;
+
+import java.util.Scanner;
+
+public class Lab064 {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        // index -> 0,1,2
+        // length -> 3
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+        marks[1] = 94;
+        marks[2] = 95;
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+        boolean[] isLearning = {true, false,false, true, true, true};
+
+        for (int i = 0; i < marks.length; i++) {
+            // i = 0,1,2
+            System.out.println(i +" -> "+ marks[i]);
+        }
+
+        String[] weekDays = {"Mon", "Tue", "Wed", "Thu"};
+        for (int i = 0; i < weekDays.length; i++) {
+            System.out.println(weekDays[i]);
+        }
+    }
+}
