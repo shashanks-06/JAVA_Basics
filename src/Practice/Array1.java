bfe27ea8c82f0dfca3cefe874e27f9b7b054f0f3
shashanks-06 <surjekarshashank@gmail.com>
2024-08-05 23:52:21 +0530

    4 August 2024

---
+++ src/Practice/Array1.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] roll = {10,20,30,40,50};
+
+        for (int i : roll){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Practice/Array2.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Array2 {
+    public static void main(String[] args) {
+        int[] a = {100,200,300,400,500,600,700,800,900};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Value of " +i+ "th element is " +a[i]);
+        }
+    }
+}
