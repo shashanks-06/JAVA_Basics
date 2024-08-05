0f7ca5370251a8141844fabd00181f0fdad6d22a
shashanks-06 <surjekarshashank@gmail.com>
2024-08-05 15:12:40 +0530

    4 August 2024

--- src/AUGUST/ex_04082024/ATBPerson.java
+++ src/AUGUST/ex_04082024/ATBPerson.java
@@ -2,10 +2,20 @@ package AUGUST.ex_04082024;
 
 public class ATBPerson {
     String name;
-    long phoneNumber;
+    long phone;
 
+    // Default Constructor
     ATBPerson(){
         System.out.println("Object is created");
+    }
+
+    // Parameterized Constructor
+    ATBPerson(String nameGiven){
+        this.name = nameGiven;
+    }
+    ATBPerson(String nameGiven, long phoneGiven){
+        this.name = nameGiven;
+        this.phone = phoneGiven;
     }
 
 }
