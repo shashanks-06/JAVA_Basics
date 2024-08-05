d23fed23cb4a6e66ad2a593b3e098adfd1544d98
shashanks-06 <surjekarshashank@gmail.com>
2024-08-05 14:49:51 +0530

    4 August 2024

---
+++ src/AUGUST/ex_04082024/ATBPerson.java
@@ -0,0 +1,11 @@
+package AUGUST.ex_04082024;
+
+public class ATBPerson {
+    String name;
+    long phoneNumber;
+
+    ATBPerson(){
+        System.out.println("Object is created");
+    }
+
+}
---
+++ src/AUGUST/ex_04082024/Car.java
@@ -0,0 +1,13 @@
+package AUGUST.ex_04082024;
+
+public class Car {
+    // Default Constructor
+    Car(){
+        System.out.println("Starting");
+        // Here, I can write anything which i want to execute before starting
+
+        System.out.println("Reading data from mySQL");
+        System.out.println("Reading data from CSV");
+        System.out.println("Reading data from Excel");
+    }
+}
