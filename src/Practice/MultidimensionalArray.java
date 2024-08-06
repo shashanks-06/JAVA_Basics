7fb2163e4c63f28ca1a96fa2b60837aa2678e1d0
shashanks-06 <surjekarshashank@gmail.com>
2024-08-06 21:31:08 +0530

    2D Array with forEach

---
+++ src/Practice/MultidimensionalArray.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class MultidimensionalArray {
+    public static void main(String[] args) {
+        int [][] a = {
+                {10,20},
+                {30,40},
+                {50,60}
+        };
+
+        //Enhanced for loop (forEach)
+        for (int[] row : a){
+            for (int columnValue : row){
+                System.out.print(columnValue + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
