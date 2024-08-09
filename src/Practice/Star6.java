2e963ee42e740e7ed0bebc0cc3f28b7c0b0592e8
shashanks-06 <surjekarshashank@gmail.com>
2024-08-09 20:32:06 +0530

    2D Array with forEach

---
+++ src/Practice/Star6.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class Star6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5  ; i++) {
+            for (int j = 1; j <=5 ; j++) {
+                if (i == 1 || j == 1 || i+j == 5 || i+j == 6 || i+j == 4){
+                    System.out.print(" * ");
+                }
+//                else if (i==2 && j == 2) {            // -> i+j == 4
+//                    System.out.print(" * ");
+//                }
+                else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
