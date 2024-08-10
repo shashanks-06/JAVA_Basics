a55991dc8b3bae08b2c5424e0e62bb491ae03ea0
shashanks-06 <surjekarshashank@gmail.com>
2024-08-10 23:09:32 +0530

    Star Patterns

---
+++ src/Practice/Star10.java
@@ -0,0 +1,37 @@
+package Practice;
+
+public class Star10 {
+    public static  void Star(int n){
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i >= 1 ; i--) {
+            for (int j = i; j >= 1 ; j--) {             // j == 3
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String []args)
+    {                                                                   // j <= i
+
+    // i >= j
+    Star(10);
+        }
+    }
+
+
+//  A  ||  B  = op
+//  1  ||  1  = 1
+// 1   ||  0  = 1
+// 0  ||   1   = 1
+// 0  || 0  = 0
+
+//  1  &&  1  = 1
+// 1   &&  0  = 0
+// 0  &&   1   = 0
+// 0  && 0  = 0
