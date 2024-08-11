8cd870711b9196051fc27adb0555ba0e40f741ff
shashanks-06 <surjekarshashank@gmail.com>
2024-08-11 14:00:00 +0530

    10th August 2024

---
+++ src/AUGUST/ex_11082024/encapsulation/Lab088.java
@@ -0,0 +1,23 @@
+package AUGUST.ex_11082024.encapsulation;
+
+// ENCAPSULATION
+public class Lab088 {
+    public static void main(String[] args) {
+        VWOLogin vwoLogin = new VWOLogin("admin","pass123" );
+        System.out.println(vwoLogin.username);
+
+        vwoLogin.username = "administrator";
+        System.out.println(vwoLogin.username);
+    }
+}
+
+class  VWOLogin{
+    public String username;
+    public String password;
+
+    // PARAMATERIZED CONSTRUCTOR
+    public VWOLogin(String uName, String passW) {
+        this.username = uName;
+        this.password = passW;
+    }
+}
---
+++ src/AUGUST/ex_11082024/encapsulation/Lab090.java
@@ -0,0 +1,24 @@
+package AUGUST.ex_11082024.encapsulation;
+
+public class Lab090 {
+    public static void main(String[] args) {
+        Axis admin = new Axis("admin", 100000);
+
+        System.out.println(admin.getUsername());        // admin
+        admin.setUsername("admin2", true);      // Allowed
+        System.out.println(admin.getUsername());        // admin2
+
+        admin.setUsername("Aniket", false);      //Not Allowed
+        System.out.println(admin.getUsername());        // admin2  ->       Remain same
+
+        // -------------------------------------------------------------------
+
+        System.out.println(admin.getBal());        // 100000
+        admin.setBal(9450360, true);      // Allowed
+        System.out.println(admin.getBal());        // 9450360
+
+        admin.setBal(9450360, false);      //Not Allowed
+        System.out.println(admin.getBal());        // 9450360    ->       Remain same
+
+    }
+}
