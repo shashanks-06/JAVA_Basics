2c697c8b33bf064e0eeab29cec617f44fb09c606
shashanks-06 <surjekarshashank@gmail.com>
2024-08-11 18:40:21 +0530

    10th August 2024

---
+++ src/AUGUST/ex_11082024/polymorphism/Lab091.java
@@ -0,0 +1,29 @@
+package AUGUST.ex_11082024.polymorphism;
+
+public class Lab091 {
+// # Polymorphism  -->  COMPILE TIME (EARLY BINDING ) || STATIC POLYMORPHISM  || METHOD OVERLOADING
+//                                                                                          |
+    //                                                        ONE OR MORE METHODS WITH SAME NAME BUT DIFFERENT ARGUMENTS
+//  RETURN TYPE DOESN'T AFFECT THE OVERLOADING
+// SAME METHOD NAME but DIFFERENT PARAMETER DATATYPE
+
+    public static void main(String[] args) {
+    MathsOperation mathsOperation = new MathsOperation();
+
+        int result1 = mathsOperation.add(2,3);
+        System.out.println(result1);
+
+        double result2 = mathsOperation.add(2.4, 3.6);
+        System.out.println(result2);
+
+        String result3 = mathsOperation.add("Shashank", "Surjekar");
+        System.out.println(result3);
+
+        String result4 = mathsOperation.add("Shashank", 611);
+        System.out.println(result4);
+
+        //  at only compilation it itself find out that parameters are not inputted as they declared in the function
+        // or such parameterized function is not present.
+    }
+
+}
