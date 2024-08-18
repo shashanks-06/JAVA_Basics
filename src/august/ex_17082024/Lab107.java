8203b769a23d991fdc57dbad729646ee883f2c4b
shashanks-06 <surjekarshashank@gmail.com>
2024-08-18 15:44:56 +0530

    17th August 2024

---
+++ src/august/ex_17082024/Lab107.java
@@ -0,0 +1,21 @@
+package august.ex_17082024;
+
+public class Lab107 {
+    public static void main(String[] args) {
+
+        APIAutomation a = new APIAutomation();
+
+        // SIB - call 1 time
+        // IIB - when object is created.
+    }
+}
+class APIAutomation{
+    static {
+        // write a code to load the data from the MySQL
+        System.out.println("Loaded data from MySQL");
+    }
+    {
+        // write a code to load the data from the CSV File
+        System.out.println("Loaded data from CSV");
+    }
+}
