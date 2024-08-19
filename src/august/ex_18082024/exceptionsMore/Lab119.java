41f12bb11c7ddddc1b7e6a28aea0e30ed1020866
shashanks-06 <surjekarshashank@gmail.com>
2024-08-19 13:05:49 +0530

    18th August 2024

--- src/august/ex_18082024/exceptionsMore/Lab119.java
+++ src/august/ex_18082024/exceptionsMore/Lab119.java
@@ -1,6 +1,19 @@
 package august.ex_18082024.exceptionsMore;
 /*      EXCEPTION PASSING        */
 
+/*
+1. Exception - Anything that disrupts the normal flow of the program.
+2. Error -> We can't handle, We have error -> halt the program -> Stackoverflow, Out of memory, IOError
+3. How many types of exceptions are there?
+    1. Checked ->  JVM knows -> FileNotFound, IOException
+    2. Unchecked -> Runtime Exceptions - NullPointer, Arithmetic, ArrayOutofIndex
+4. Throwable
+    1. Exception -> Runtime -> Unchecked ->  Null Pinter, Arithmetic
+                    Compile Time - Checked -> IOException, SQLeEXCEPTION
+    2. Error -> Stackoverflow
+*/
+
+
 public class Lab119 {
     public static void main(String[] args) {
         extracted();
