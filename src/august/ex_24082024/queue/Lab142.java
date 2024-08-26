c856cd8714a3df1d7ab2a999392f66e0a0048cd2
shashanks-06 <surjekarshashank@gmail.com>
2024-08-26 18:17:31 +0530

    24th August 2024

---
+++ src/august/ex_24082024/queue/Lab142.java
@@ -0,0 +1,21 @@
+package august.ex_24082024.queue;
+
+import java.util.PriorityQueue;
+
+public class Lab142 {
+    public static void main(String[] args) {
+        //  QUEUE ->  Used less than 1%
+
+        PriorityQueue pq = new PriorityQueue();
+        pq.offer("1");      // offer = add
+        pq.offer("4");
+        pq.offer("2");
+        pq.offer("3");
+
+        System.out.println(pq);             // [1, 3, 2, 4]
+        System.out.println(pq.peek());      // 1
+        System.out.println(pq.poll());      // 1
+        System.out.println(pq);             // [2, 3, 4]
+        
+    }
+}
