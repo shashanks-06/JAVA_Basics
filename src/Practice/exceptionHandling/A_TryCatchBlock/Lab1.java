8f32217828ed019a7b85e3fb57eefe232a0f9cdf
shashanks-06 <surjekarshashank@gmail.com>
2024-09-02 22:54:35 +0530

    ArithmeticException

--- src/Practice/exceptionHandling/A_TryCatchBlock/Lab1.java
+++ src/Practice/exceptionHandling/A_TryCatchBlock/Lab1.java
@@ -6,8 +6,11 @@ public class Lab1 {
             int a = 100/0;
             System.out.println(a);
         }catch (ArithmeticException e){
-            System.out.println(e.toString());
+            System.out.println(e.toString());   // java.lang.ArithmeticException: / by zero
         }
-        System.out.println("Hello JAVA");
+        //        finally {
+//            System.out.println("Remaining Code");
+//        }
+        System.out.println("Remaining Code");
     }
 }
