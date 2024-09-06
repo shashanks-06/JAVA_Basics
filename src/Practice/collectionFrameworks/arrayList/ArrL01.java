45cdf1b8f1ae49dc572dccce63a7d17c1b56c6e1
shashanks-06 <surjekarshashank@gmail.com>
2024-09-06 21:25:23 +0530

    Collection - ArrayList

---
+++ src/Practice/collectionFrameworks/arrayList/ArrL01.java
@@ -0,0 +1,62 @@
+package Practice.collectionFrameworks.arrayList;
+
+import java.util.ArrayList;
+
+public class ArrL01 {
+    public static void main(String[] args) {
+        ArrayList<Object> arrayList = new ArrayList<>();
+//        List<> list = new ArrayList<>();
+
+        arrayList.add("Amravati");
+        arrayList.add(611);
+        arrayList.add(true);
+        arrayList.add('s');
+        arrayList.add(02.07);
+        arrayList.add(null);
+
+        System.out.println(arrayList);
+        System.out.println("Size of ArrayList - " + arrayList.size());
+        System.out.println("Is ArrayList Empty: " + arrayList.isEmpty());
+        System.out.println(arrayList.get(4));
+        System.out.println(arrayList.indexOf(611));
+
+        arrayList.add(false);
+        System.out.println(arrayList);
+        arrayList.remove(5);
+        System.out.println(arrayList);
+
+        arrayList.add(2, "Shashank");
+        System.out.println(arrayList);
+
+        arrayList.set(1, 60101);
+        System.out.println(arrayList);
+
+        System.out.println("----------------------------------------------------------");
+
+        ArrayList<Object> arrayList2 = new ArrayList<>();
+        arrayList2.add("Amravati");
+        arrayList2.add(60101);
+        arrayList2.add("Shashank");
+        System.out.println(arrayList2);
+        System.out.println(arrayList);
+
+        arrayList.removeAll(arrayList2);
+        System.out.println(arrayList);
+
+/*
+        System.out.println("----------------------------------------------------------");
+
+        for (Object o : arrayList){
+            System.out.println(o);
+        }
+
+        System.out.println("----------------------------------------------------------");
+
+        Iterator<Object> itr = arrayList.iterator();
+        while (itr.hasNext()){
+            System.out.print(itr.next()+" ");
+        }
+*/
+
+    }
+}
