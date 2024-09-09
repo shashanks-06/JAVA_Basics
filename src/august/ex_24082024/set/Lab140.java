d87f1d34e45c98fa831d049dc9462cce11d4999b
shashanks-06 <surjekarshashank@gmail.com>
2024-09-09 21:05:48 +0530

    24th July 2024

--- src/august/ex_24082024/set/Lab140.java
+++ src/august/ex_24082024/set/Lab140.java
@@ -7,7 +7,7 @@ public class Lab140 {
         // List - > Duplicates Allows
         // Set -> Doesn't allow Duplicates
 
-        Set hs = new HashSet(); // Hashing mechanism to store the element, NO ORDER
+        HashSet hs = new HashSet(); // Hashing mechanism to store the element, NO ORDER
         hs.add("Apple");
         hs.add("apple");
         hs.add("Apple");
@@ -16,37 +16,39 @@ public class Lab140 {
         hs.add(null);
         System.out.println(hs);     // [apple, Apple, Watermelon, Orange]
 
-        Set lhs = new LinkedHashSet();  // LinkedList mechanism to store the element, MAINTAIN ORDER
-        lhs.add("Apple");
-        lhs.add("Orange");
-        lhs.add("Watermelon");
-        lhs.add("watermelon");
-        lhs.add("Watermelon");
-        lhs.add(null);
-        System.out.println(lhs);    // [Apple, Orange, Watermelon, watermelon]
-        System.out.println(lhs.isEmpty());
-        System.out.println(lhs.contains("Apple"));
-        System.out.println(lhs.size());
+//        Set lhs = new LinkedHashSet();  // LinkedList mechanism to store the element, MAINTAIN ORDER
+//        lhs.add("Apple");
+//        lhs.add("Orange");
+//        lhs.add("Watermelon");
+//        lhs.add("watermelon");
+//        lhs.add("Watermelon");
+//        lhs.add(null);
+//        System.out.println(lhs);    // [Apple, Orange, Watermelon, watermelon]
+//        System.out.println(lhs.isEmpty());
+//        System.out.println(lhs.contains("Apple"));
+//        System.out.println(lhs.size());
+//
+//        Set ts = new TreeSet();  // Black and Red Tree mechanism to store the element, MAINTAIN ORDER
+//        ts.add("Dapple");       // Automatically sorted
+//        ts.add("Apple");
+//        ts.add("Orange");
+//        ts.add("Orange");
+//        ts.add("Watermelon");
+////        ts.add(null);  -> # can't add null in TreeSet as it sort the elements, and null can't be sorted
+//        System.out.println(ts+"\n");     // [Apple, Dapple, Orange, Watermelon]
+//
+//          Can't use the normal "for-loop" as the data in the Set is not stored by index value
+//          So Enhanced for-loop or Iterator is used to print values
 
-        Set ts = new TreeSet();  // Black and Red Tree mechanism to store the element, MAINTAIN ORDER
-        ts.add("Dapple");       // Automatically sorted
-        ts.add("Apple");
-        ts.add("Orange");
-        ts.add("Orange");
-        ts.add("Watermelon");
-//        ts.add(null);  -> # can't add null in TreeSet as it sort the elements, and null can't be sorted
-        System.out.println(ts+"\n");     // [Apple, Dapple, Orange, Watermelon]
-
-
-        for(Object o : ts){
-            System.out.println(o);
-        }
-
-        System.out.println();
-
-        Iterator it = ts.iterator();
-        while (it.hasNext()){
-            System.out.println(it.next());
-        }
+//        for(Object o : ts){
+//            System.out.println(o);
+//        }
+//
+//        System.out.println();
+//
+//        Iterator it = ts.iterator();
+//        while (it.hasNext()){
+//            System.out.println(it.next());
+//        }
     }
 }
