ed9aef4fc75fd449a66847f45ded8d424b331781
shashanks-06 <surjekarshashank@gmail.com>
2025-01-08 23:48:20 +0530

    diagonal @ matrix in * matrix

--- src/Practice/starPatterns/Star4.java
+++ src/Practice/starPatterns/Star4.java
@@ -5,9 +5,9 @@ public class Star4 {
         for (int i = 1; i <=5 ; i++) {
             for (int j = 1; j <= 5 ; j++) {
                 if (i==j || i==5 || j==1){
-                    System.out.print(" * ");
+                    System.out.print(" @ ");
                 } else if ((i==4 && (j==2 || j==3)) || j==2 && i==3) {
-                    System.out.print(" * ");
+                    System.out.print(" @ ");
                 }else {
                     System.out.print(" * ");
                 }
@@ -16,8 +16,8 @@ public class Star4 {
         }
     }
 }
-// *  *  *  *  *
-// *  *  *  *  *
-// *  *  *  *  *
-// *  *  *  *  *
-// *  *  *  *  *
+// @  *  *  *  *
+// @  @  *  *  *
+// @  @  @  *  *
+// @  @  @  @  *
+// @  @  @  @  @
